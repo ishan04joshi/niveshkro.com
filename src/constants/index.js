e42766080cf8dea497ec063dcc60bb57752d3200
ishan04joshi <90335897+ishan04joshi@users.noreply.github.com>
2023-03-25 19:14:26 +0530

    .

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+// !! PRODUCTION
+// export const websiteURL = "https://niveshkro.com";
+// export const dashboardURL = "https://app.niveshkro.com";
+
+// !! STAGING
+
+// export const WEBSITE_URL = "https://staging.niveshkro.com";
+// export const PORTAL_URL = "https://staging.app.niveshkro.com";
+// export const backendURL = "https://api.staging.niveshkro.com"
+
+// !! DEVELOPMENT
+
+export const websiteURL = "https://nivesh-landing-new.netlify.app/";
+export const dashboardURL = "https://app-niveshkro-new.netlify.app/";
+export const backendURL = "https://api.staging.niveshkro.com";
